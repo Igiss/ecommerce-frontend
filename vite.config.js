8b8c3f621ed06113dd70afe378a5d21c9237ef00
Nguyễn Niệm <niem.nguyen@itbeesolutions.com>
2026-06-06 12:36:02 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000, proxy: { '/api': 'http://localhost:4000' } }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_DEV_PORT) || 3005,
+      proxy: {
+        '/api': env.VITE_API_URL || 'http://localhost:3000'
+      }
+    }
+  }
 })
