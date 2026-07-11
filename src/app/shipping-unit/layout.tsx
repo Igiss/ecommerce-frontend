9d23b13b44e6ca85ba62f02958311bd779b47ff3
TranDangDuy25 <trandangduydmx@gmail.com>
2026-07-11 14:29:37 +0700

    giao dien shipper

--- src/app/shipping-unit/layout.tsx
+++ src/app/shipping-unit/layout.tsx
@@ -125,7 +125,7 @@ export default function ShippingUnitLayout({ children }: ShippingUnitLayoutProps
 
       {/* Main Content Area */}
       <main className="flex-1 overflow-y-auto h-screen p-8">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-7xl mx-auto">
           {children}
         </div>
       </main>
